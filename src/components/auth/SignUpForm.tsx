115d927a169faae9058e7b7c7c972d39559d3bab
Yannn <tiantramdan07@gmail.com>
2025-12-29 15:40:53 +0700

    first commit

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -184,9 +184,20 @@ export default function SignUpForm() {
                 </div>
               </div>
 
-              <div className="flex items-center gap-3">
-                <Checkbox className="w-5 h-5" checked={isChecked} onChange={setIsChecked} />
-                <p className="inline-block font-normal text-gray-500 dark:text-gray-400">By creating an account, you agree to the <span className="text-gray-800 dark:text-white/90">Terms and Conditions</span> and our <span className="text-gray-800 dark:text-white">Privacy Policy</span></p>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <Checkbox 
+                    checked={isChecked} 
+                    onChange={setIsChecked} 
+                    className="w-4 h-4 rounded border-gray-400 dark:border-gray-500 data-[state=checked]:bg-gray-800 dark:data-[state=checked]:bg-white"
+                  />
+                  <p className="font-normal text-gray-700 text-theme-sm dark:text-gray-400">
+                    By creating an account, you agree to the{" "}
+                    <span className="text-gray-900 dark:text-white">Terms and Conditions</span>{" "}
+                    and our{" "}
+                    <span className="text-gray-900 dark:text-white">Privacy Policy</span>
+                  </p>
+                </div>
               </div>
 
               <div>
