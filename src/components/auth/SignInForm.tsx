0f808efbecf52160e3185938e54359eb2ef64966
Yannn <tiantramdan07@gmail.com>
2025-12-29 10:53:05 +0700

    first commit

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -183,7 +183,7 @@ export default function SignInForm() {
             <p className="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start">
               Don't have an account?{" "}
               <Link
-                to="/AiScale/signup"
+                to="/signup"
                 className="text-brand-500 hover:text-brand-600 dark:text-brand-400"
               >
                 Sign Up
