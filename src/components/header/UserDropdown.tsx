bcf7a21e5c37bfd52f69bc62c5b81a617b41fb76
Yannn <tiantramdan07@gmail.com>
2026-01-02 16:57:48 +0700

    first commit

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -70,7 +70,7 @@ export default function UserDropdown() {
         {/* FOTO USER */}
         <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
           <img
-            src={user?.avatar || "./images/user/profileuser.png"}
+            src={user?.avatar || "./images/user/user-01.jpg"}
             alt="User"
           />
         </span>
