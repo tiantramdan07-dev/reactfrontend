e154338346e1777510099b6c5a8a06518f2a16e3
Yannn <tiantramdan07@gmail.com>
2026-02-09 10:28:14 +0700

    first commit

--- src/pages/Dashboard/ScaleDashboard.tsx
+++ src/pages/Dashboard/ScaleDashboard.tsx
@@ -10,6 +10,7 @@ interface Produk {
 }
 
 const API_URL = "http://192.168.10.215:4000"; // bisa taruh di .env (VITE_API_URL)
+const [now, setNow] = useState<Date>(new Date());
 
 /**
  * Hapus emoji / simbol dari pesan server.
@@ -43,6 +44,15 @@ const ScaleDashboard: React.FC = () => {
       .then((data) => setProducts(data))
       .catch((err) => console.error("❌ Gagal memuat produk:", err));
   }, []);
+
+useEffect(() => {
+  const timer = setInterval(() => {
+    setNow(new Date());
+  }, 1000);
+
+  return () => clearInterval(timer);
+}, []);
+
 
   // Update status timbangan & deteksi tiap 1 detik
 useEffect(() => {
